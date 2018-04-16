dc1272013dbfcd64d0683c895a8a613849d960b2
WEIMAR <alexandertorreshe@gmail.com>
2018-04-16 09:08:56 -0400

    Login xml 1

--- app/src/main/java/com/programacion3/androidlatte/app/MainActivity.java
+++ app/src/main/java/com/programacion3/androidlatte/app/MainActivity.java
@@ -1,13 +1,22 @@
 package com.programacion3.androidlatte.app;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textNuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textNuevo = findViewById(R.id.nuevo);
+
+        textNuevo.setPaintFlags(textNuevo.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
     }
 }
